07ff9606c7292c383d75318ae50371a02dfdb638
jtw <jtw7913@gmail.com>
2026-02-23 00:30:18 +0900

    fix: db이름 변경

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,7 +4,7 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const dbPath = path.join(__dirname, "trade.db");
+const dbPath = path.join(__dirname, "discord-bot.db");
 
 const db = new Database(dbPath);
 
